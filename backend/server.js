05a6a5b8fbdcbc6b3a7bf49955400bdb203caf55
7jayasri <jayasri73737@gmail.com>
2026-06-02 14:13:58 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "http://127.0.0.1:5500",
+    "https://sports-scheduler-frontend.netlify.app"
+  ]
+}));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
